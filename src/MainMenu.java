0bc3de87f7f17ef81cc6cc7cb614e99333be8264
jonathangibson1991 <jonathangibson1991@gmail.com>
2018-03-31 15:17:28 +0100

    Set up main menu

---
+++ src/MainMenu.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by jonat on 31/03/2018.
+ */
+public class MainMenu {
+    private JPanel MainPanel;
+    private JButton stockManagementButton;
+    private JButton customerManagementButton;
+    private JButton saleHistoryButton;
+    private JButton saleButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
