9ac38bc7809737ababc91b597fa46fd846ac0a35
jonathangibson1991 <jonathangibson1991@gmail.com>
2018-04-03 14:00:13 +0100

    Add Customer GUI Complete

--- src/AddCustomer.java
+++ src/AddCustomer.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class AddCustomer {
+    private JPanel AddCustomerPanel;
+    private JTextField FirstNameField;
+    private JTextField LastNameField;
+    private JTextField PhoneNumberField;
+    private JButton AddButton;
+    private JLabel FirstNameLabel;
+    private JLabel LastNameLabel;
+    private JLabel PhoneNumberLabel;
 }
---
+++ src/AddCustomerDataHandler.java
@@ -0,0 +1,35 @@
+import java.sql.DriverManager;
+
+/**
+ * Created by jonat on 03/04/2018.
+ */
+public class AddCustomerDataHandler {
+    private static java.sql.Connection connection;
+    private static java.sql.Statement statement;
+
+    public static void addCustomer (String firstName, String lastName, String phoneNum) {
+        String sqlQuery = "INSERT INTO Customers (FirstName, LastName, PhoneNumber) VALUES ('" + firstName +"', '" + lastName +"', '" + phoneNum +"')";
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(DbCredentials.dbUrl, DbCredentials.dbUsername, DbCredentials.dbPassword);
+            statement = connection.createStatement();
+            int Result = statement.executeUpdate(sqlQuery);
+        }
+        catch ( ClassNotFoundException cnfex ) {
+            System.err.println("Issue with driver." );
+            cnfex.printStackTrace();
+            System.exit( 1 ); // terminate program
+        }
+        catch ( java.sql.SQLException sqlex ) {
+            System.err.println( sqlex );
+            sqlex.printStackTrace();
+        }
+        catch ( Exception ex ) {
+            System.err.println( ex );
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+}
